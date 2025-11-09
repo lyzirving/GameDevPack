6f92ec9200bbf74d60da2b6f2a77cb8b514eb606
lyzirving <lyzirving@sina.com>
2025-11-09 21:58:44 +0800

    add IState.
    Signed-off-by: lyzirving <lyzirving@sina.com>

---
+++ Assets/Scripts/State/IState.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace MovementSystem
+{
+    public interface IState
+    {
+        public void Enter();
+        public void Exit();
+
+        public void Update();
+
+        public void PhysicsUpdate();
+
+        public void HandleInput();
+    }
+}
---
+++ Assets/Scripts/State/StateMachine.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+
+namespace MovementSystem
+{
+    public abstract class StateMachine
+    {
+        protected IState m_CurrentState;
+
+        public void ChangeState(IState newState)
+        {
+            m_CurrentState?.Exit();
+            m_CurrentState = newState;
+            m_CurrentState?.Enter();
+        }
+
+        public void Update()
+        {
+            m_CurrentState?.Update();
+        }
+
+        public void PhsicsUpdate()
+        {
+            m_CurrentState?.PhysicsUpdate();
+        }
+
+        public void HandleInput()
+        {
+            m_CurrentState?.HandleInput();
+        }
+    }
+}
